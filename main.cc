5400c87ad00356dc03f13603f0a2f8f5d2d62dbb
JINGUJI <5592048+jin0g@users.noreply.github.com>
2023-10-08 10:48:12 +0900

    Add AB-test

--- main.cc
+++ main.cc
@@ -6,5 +6,16 @@
 int kernel(int a, int b, int c) {
 #pragma HLS pipeline II=1
 #pragma HLS interface ap_ctrl_none port=return
-  return a + b * c;
+
+    ap_int<32> tmp = a + b * c;
+
+#ifdef TEST_A
+    tmp = a + ap_uint<8>(b) * ap_uint<8>(c);
+#endif
+
+#ifdef TEST_B
+    tmp = a + ap_int<8>(b) * ap_int<8>(c);
+#endif
+
+    return tmp;
 }
